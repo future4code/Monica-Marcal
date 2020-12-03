e56ec905b20a184af6e6c489438336df0b89145d
MonicaMarcal <monicagmarcal@outlook.com>
2020-12-03 16:42:51 -0300

    semana3 aula12 - loops

---
+++ semana3/loops/exercicioInterpretacao.js
@@ -0,0 +1,31 @@
+/*
+
+EXERCÍCIO 1
+
+O que o código abaixo está fazendo? Qual o resultado impresso no console?
+//se o numero for maior que zero conte +1 por cinco vezes
+
+let valor = 0
+for(let i = 0; i < 5; i++) {
+  valor += i
+}
+console.log(valor)
+*/
+
+/*
+
+EXERCÍCIO 2
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+}
+
+
+a. O que vai ser impresso no console?
+//os numeros maiores que 18 do array
+b. Se eu quisesse acessar o **índice** de cada elemento dessa lista, o `for...of...` é suficiente? Se sim, o que poderia ser usado para fazer isso?
+*/
+
+
