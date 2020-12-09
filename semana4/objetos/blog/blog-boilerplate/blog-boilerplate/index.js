fccc982bafc62838aaaeaf512ab5a7f3941cda26
MonicaMarcal <monicagmarcal@outlook.com>
2020-12-09 16:21:56 -0300

    semana4-aula15 -Objects

---
+++ semana4/objetos/blog/blog-boilerplate/blog-boilerplate/index.js
@@ -0,0 +1,38 @@
+/*2 - Capture os valores dos campos do HTML e crie um objeto para armazenar as informações do seu post, possuindo as mesmas propriedades que serão preenchidas no formulário.
+
+3 - Ao preencher o formulário, adicione o objeto criado no passo acima a um array de objetos e limpe os campos do formulário. Para testar, faça com que o array seja impresso no console.
+
+4 - **Na mesma página**, crie uma seção no HTML que será responsável por mostrar os posts. Pode ser uma div ou uma section, por exemplo.
+
+5 - Crie uma função responsável por inserir os posts no HTML. Não se preocupe com a aparência, garanta que os dados estão sendo exibidos.*/
+
+
+
+/*
+    function apertouEnter() {
+         const formulario = document.getElementById("formulario")
+         const text = document.getElementById("text")
+         formulario.innerHTML = `<section>${text.value}</section>`
+         text.value = ""
+     }
+/*
+
+function blog() {
+        const formulario = document.getElementById("formulario")
+         const text = document.getElementById("text")
+         formulario.innerHTML = `<section>${text.value}</section>`
+         text.value = ""
+     }
+    
+     function apertouBotao(evento){
+         console.log(evento)
+         blog()
+ }
+    
+     function apertouEnter(evento){
+         if (evento.key === 'Criar Post'){
+            blog()
+         }
+     }
+
+     */
