db6e57d8a7d1a7eaf8c3321ade2e17b0dc00c04c
MonicaMarcal <monicagmarcal@outlook.com>
2020-12-11 16:05:34 -0300

    Exercicio de interpretação de código

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,79 @@
+//EXERCICIO DE INTERPRETAÇÃO
+
+//1.
+//função que irá converter dolar para reais
+//variavel cotação está pegando o valor que o usuario digitar
+//retornando valor da cotação vezes conversor de moeda   
+//variavel meuDinheiro igual a 100
+//retorna o valor do dinheiro vezes a cotação
+
+
+//2. 
+//Função que receberá um calculo de tipos de investimentos
+//criou variavel que recebrá o valor dos casos abaixo
+// switch criou opções e cada uma recebe o valor de investimento , vezes.
+//default, caso o usuario escolha uma opcao nao existente 
+//return, retornando o valor escolhido do usuario ex: "Ações"
+//criando outras duas variaveis que gaurad o valor recebido das ações e investe 150 e 200.
+//retornando valores investidos do novoMontante e segundoMontante.
+
+//3. 
+//declarando varios numeros dentro de um array 
+//declarando dois arrays vazios
+//se numero for resto zero
+//imprima os pares 
+//senao os impares
+//imprimi o total dos numeros do array numeros 
+//imprime o total de numeros pares 
+//imprime o total de numeros impares 
+
+//4. 
+//declarando varios numeros dentro de um array 
+//criando uma variavel com valor infinito
+//criando uma variavel com valor zero
+//não entendi
+
+
+//EXERCICIOS DE LÓGICA DE PROGRAMAÇÃO
+//1.
+
+//2. 
+//A) true E false E true 
+//B) (true E false) OU true
+//C) (false E true) E (false OU true)
+//D) (true E false) OU (false E false)
+//E) (false) E (true) OU (true E false E false)
+
+//3.
+
+/*
+
+
+const quantidadeDeNumerosPares
+let num = 0;
+while(num <= quantidadeDeNumerosPares){
+    const numero = Number(prompt("Insira um número :"))
+   
+}
+
+console.log(quantidadeDeNumerosPares)
+*/
+// // Teste: 10, 3, 50, 7, 0 => resultado 70
+//  let naoRecebeuZero = true
+//  let soma = 0
+
+//  while (naoRecebeuZero){
+//      const numero = Number(prompt("Insira um número diferente de 0 para somar:"))
+//      soma = soma + numero
+//      if (numero === 0){
+//          naoRecebeuZero = false
+//      } 
+//  }
+
+//  console.log("Sua soma é: ", soma)
+
+//5.
+
+
+
+
