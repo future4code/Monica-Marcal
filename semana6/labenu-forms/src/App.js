a166b20a76fb771f0ff356568f2711e9532c1f0a
MonicaMarcal <monicagmarcal@outlook.com>
2021-01-05 12:38:26 -0300

    atualizacao 1

--- semana6/labenu-forms/src/App.js
+++ semana6/labenu-forms/src/App.js
@@ -1,13 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Etapa1</h1>
-     
-    </div>
-  );
+import React from 'react';
+import Etapa1 from "./components/etapa1"
+import Etapa2 from "./components/etapa2"
+import Etapa3 from "./components/etapa3"
+import Final from "./components/final"
+
+
+class App extends React.Component {
+  state = {
+    etapa: 1,
+    etapa: 2,
+    etapa: 3,
+    final: Final
+  }
+
+  renderizaEtapa = () => {
+    switch (this.state.evento) {
+      case 1: 
+        return <Etapa1 />;
+      case 2: 
+        return <Etapa2 />;
+      case 3: 
+        return <Etapa3 />;
+      case Final: 
+        return <Final />;
+    }
+  }
+
+ 
+  
+
+render() {
+	return (
+	      <div>
+					{this.renderizaEtapa()}
+	        <button>Próxima Etapa</button>
+	      </div>
+	    );
+}
 }
 
-export default App;
