e8fb645fda328f2d92ddf647d02615762349ff66
MonicaMarcal <monicagmarcal@outlook.com>
2021-01-14 16:34:34 -0300

    Integracao com API

---
+++ semana7/integra-api/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import axios from 'axios'
+
+export default class App extends React.Component {
+  state = {
+
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
